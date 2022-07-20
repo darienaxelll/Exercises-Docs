1688d3f87df919cc6be95bf0f485742e41c41ef3
Darien Salazar <darien.axell.salazar@gmail.com>
2022-07-20 13:45:31 -0500

    completed github exercise

---
+++ js/promises.js
@@ -0,0 +1,21 @@
+let userInput = document.querySelector('#userInput');
+let btn = document.querySelector('#submit');
+
+function action() {
+
+    function githubUser() {
+        return fetch(`https://api.github.com/users/${userInput.value}/events/public`, {headers: {'Authorization': gitHubKey}})
+            .then(response => response.json())
+    }
+
+    githubUser().then (info  => {
+        console.log(info)
+        console.log(info[0].created_at);
+
+        // let userDetails = JSON.parse(info);
+        // console.log(userDetails[0].created_at);
+    })
+}
+
+btn.addEventListener('click', action);
+
