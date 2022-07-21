67f3b0854332eafb0abd3afc4b5b826b2bd46ba2
Darien Salazar <darien.axell.salazar@gmail.com>
2022-07-21 15:33:39 -0500

    edited home page

--- jalopy-main/js/views/Home.js
+++ jalopy-main/js/views/Home.js
@@ -5,26 +5,123 @@ const BASE_URI = `${BACKEND_HOST}/api/s3/download`;
 
 export default function Home(props) {
     return `
-        <header>
-            <h1>Home Page</h1>
-        </header>
-        <main>
-            <div>
-                <p>
-                    This is the home page text.
-                </p>    
+    <header>
+        <style>
+            .slideshow-container {
+              max-width: 100%;
+              height: 300px;
+              position: relative;
+              margin: auto;
+            }
+            
+            .mySlides {
+              display: none;
+            }
+            
+            .prev, .next {
+              cursor: pointer;
+              position: absolute;
+              top: 50%;
+              width: auto;
+              margin-top: -22px;
+              padding: 16px;
+              color: white;
+              font-weight: bold;
+              font-size: 18px;
+              transition: 0.6s ease;
+              border-radius: 0 3px 3px 0;
+              user-select: none;
+            }
+            
+            .next {
+              right: 0;
+              border-radius: 3px 0 0 3px;
+            }
+            
+            .prev:hover, .next:hover {
+              background-color: rgba(0,0,0,0.8);
+            }
+            
+            .text {
+              margin-bottom: 60px;
+              color: #f2f2f2;
+              font-size: 15px;
+              padding: 8px 12px;
+              position: absolute;
+              bottom: 8px;
+              width: 100%;
+              text-align: center;
+            }
+            
+            .dot {
+              cursor: pointer;
+              height: 15px;
+              width: 15px;
+              margin: 0 2px;
+              background-color: #bbb;
+              border-radius: 50%;
+              display: inline-block;
+              transition: background-color 0.6s ease;
+            }
+            
+            .active, .dot:hover {
+              background-color: #717171;
+            }
+            
+            .fade {
+              animation-name: fade;
+              animation-duration: 1.5s;
+            }
+            
+            @keyframes fade {
+              from {opacity: .4} 
+              to {opacity: 1}
+            }
+        </style>
+    </header>
+    
+    <body>
+       <div class="slideshow-container fade">
+            <div class="mySlides fade">
+                 <img src="../../assets/jalopy1.jpeg" alt="car1" style = "width:100%">
+                 <div class="text">Inset Text Here ...</div>
             </div>
-        </main>
+            
+             <div class="mySlides fade">
+                 <img src="../../assets/jalopy2.jpeg" alt="car2" style = "width:100%">
+                 <div class="text">Inset Text Here ...</div>
+            </div>
+            
+            <div class="mySlides fade">
+                 <img src="../../assets/jalopy3.jpeg" alt="car3" style = "width:100%">
+                 <div class="text">Inset Text Here ...</div>
+            </div>
+            
+            <div style="text-align:center">
+              <span class="dot"></span> 
+              <span class="dot"></span> 
+              <span class="dot"></span> 
+            </div>
+            
+       </div>
+    </body>
+
     `;
 }
 
 export function HomeEvents() {
-    // TODO: use an enum for message type
-    // const authority = getUserRole();
-    const user = getUser();
-    if(!user) {
-        showNotification("Welcome visitor", "secondary");
-    } else {
-        showNotification("Welcome " + user.userName, "info");
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 4000); // Change image every 2 seconds
     }
 }
--- jalopy-main/js/views/partials/Navbar.js
+++ jalopy-main/js/views/partials/Navbar.js
@@ -5,11 +5,72 @@ export default function Navbar(props) {
 
     // everyone can see home
     let html = `
+         <style>
+            nav {
+                display: flex;
+                padding: 10px;
+                flex-direction: row;
+                justify-content: space-between;
+                margin-bottom: 10px;
+            }
+            
+            a {
+                text-decoration: none;
+                color: #2f2fdc;
+                padding: 5px 13px;
+                font-family: 'Montserrat', sans-serif;
+                font-size: medium;
+            }
+            
+            #logo {
+                font-size: xx-large;
+                color: darkorange;
+                font-family: 'Roboto Slab', serif;
+                margin-right: 800px;
+            }
+            
+            #logo:hover {
+                background-color: #E6ECF0;
+                border-radius: 10px;
+            }
+            
+            #login {
+            margin-left: 20px;
+                border: 2px solid #2c2ca1;
+                border-radius: 50px;
+                color: #2c2ca1;
+            }
+            
+            #login:hover {
+                border: 2px solid #7676ff;
+                border-radius: 50px;
+                color: #7676ff;
+            }
+            
+            #about:hover {
+                background-color: #E6ECF0;
+                border-radius: 50px;
+            }
+            
+            #register {
+                background-color: #2c2ca1;
+                border: 1px solid #2c2ca1;
+                border-radius: 50px;
+                color: white;
+            }
+            
+            #register:hover {
+                background-color: #7676ff;
+                border: 1px solid #7676ff;
+                border-radius: 50px;
+            }
+        </style>
+        
         <nav>
-            <a class="jalopy-nav" href="/" data-link>Home</a>`;
+            <a class="jalopy-nav" href="/" id="logo" data-link>Jalopy... <i class="fas fa-truck-pickup"></i></a>`;
 
     // everyone can see about
-    html = html + `<a class="jalopy-nav" href="/about" data-link>About</a>`;
+    html = html + `<a class="jalopy-nav" href="/about" data-link id="about">About</a>`;
 
     // only logged in can see user info and logout
     if(loggedIn) {
@@ -17,8 +78,8 @@ export default function Navbar(props) {
             <a href="/logout" data-link>Logout</a>`;
     } else {
         // if not logged in, can see login and register
-        html = html + `<a class="jalopy-nav" href="/login" data-link>Login</a>
-        <a class="jalopy-nav" href="/register" data-link>Register</a>`;
+        html = html + `<a class="jalopy-nav" href="/login" id="login" data-link>Login <i class="fas fa-user-circle"></i></a>
+        <a class="jalopy-nav" href="/register" id="register" data-link>Register</a>`;
     }
 
     html = html + `</nav>`;
