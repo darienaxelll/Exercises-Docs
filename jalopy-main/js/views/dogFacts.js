09c519d3af7e3541b2666904a646409134df7936
Darien Salazar <darien.axell.salazar@gmail.com>
2022-07-25 11:08:20 -0500

    modified dogFacts view with API

--- jalopy-main/js/views/dogFacts.js
+++ jalopy-main/js/views/dogFacts.js
@@ -3,40 +3,64 @@ let txt = 'Dog Facts...';
 let speed = 700;
 
 export default function DogFactsView(props) {
-    for (let j = 0; j < props.dogFacts.length; j++) {
-        document.querySelector('#clickMe').addEventListener('click', function (event) {
-            document.querySelector('#container').innerHTML += props.dogFacts[j];
-        })
-    }
+    let html = `
+        <header>
+            <style>
+                #headerDiv {
+                    display: flex;
+                    justify-content: center;
+                    font-size: xx-large;
+                    margin-bottom: 20px;
+                }
+                 
+                 #btnDiv {
+                     display: flex;
+                     justify-content: center;
+                     margin-top: 20px;
+                 }
+                 
+                 p {
+                    padding: 10px;
+                 }
+                 
+                 #pDiv {
+                    border: 1px solid black;
+                    border-radius: 10px;
+                    background-color: #F5F5DC;
+                    margin: 5px 0;
+                 }
+                 
+            </style>
+        </header>
 
-    return `
-    <header>
-        <style>
-
-        </style>
-    </header>
-    
-    <body>
         <div>
-            <h1 id="header"></h1>
+            <div id="headerDiv"></div>
+            
+            `;
+            for(let j = 0; j < props.dogFacts.length; j++) {
+                html += `
+                <div id="pDiv">
+                    <p>${props.dogFacts[j]}</p>
+                </div>
+                `
+            }
+            html += `
+            <div id="btnDiv">
+                <button>Show Fact</button>
+            </div>
         </div>
-        
-        <div class="container"></div>
-        <button id="clickMe">Click Me</button>
-    
-    </body>
-`;
+        `;
+    return html;
 }
 
 function typeWriter() {
     if (i < txt.length) {
-        document.getElementById('header').innerHTML += txt.charAt(i);
+        document.getElementById('headerDiv').innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
 }
 
-
 export function DogFactsEvents() {
-    typeWriter()
+    typeWriter();
 }
